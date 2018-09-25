d88add3190452e277fbe58521a79d40b9d559c91
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2018-09-25 16:17:26 +0200

    [FIX] base_rest, base_rest_demo: Fix exceptions handling

--- base_rest_demo/tests/test_exception.py
+++ base_rest_demo/tests/test_exception.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2018 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 import json
@@ -18,20 +17,20 @@ class TestException(HttpCase, RegistryMixin):
         super(TestException, cls).setUpClass()
         cls.setUpRegistry()
         host = '127.0.0.1'
-        port = odoo.tools.config['xmlrpc_port']
+        port = odoo.tools.config['http_port']
         cls.url = 'http://%s:%d/base_rest_demo_api/public/exception' % (
             host, port)
 
     def setUp(self):
         super(TestException, self).setUp()
-        self.opener.addheaders.append(('Content-Type', 'application/json'))
+        self.opener.headers['Content-Type'] = 'application/json'
 
     @odoo.tools.mute_logger('odoo.addons.base_rest.http')
     def test_user_error(self):
         response = self.url_open('%s/user_error' % self.url, "{}")
-        self.assertEqual(response.code, 400)
+        self.assertEqual(response.status_code, 400)
         self.assertEqual(response.headers['content-type'], 'application/json')
-        body = json.loads(response.fp.read())
+        body = json.loads(response.content)
         self.assertDictEqual(
             body,
             {"code": 400,
@@ -42,9 +41,9 @@ class TestException(HttpCase, RegistryMixin):
     @odoo.tools.mute_logger('odoo.addons.base_rest.http')
     def test_validation_error(self):
         response = self.url_open('%s/validation_error' % self.url, "{}")
-        self.assertEqual(response.code, 400)
+        self.assertEqual(response.status_code, 400)
         self.assertEqual(response.headers['content-type'], 'application/json')
-        body = json.loads(response.fp.read())
+        body = json.loads(response.content)
         self.assertDictEqual(
             body,
             {"code": 400,
@@ -55,9 +54,9 @@ class TestException(HttpCase, RegistryMixin):
     @odoo.tools.mute_logger('odoo.addons.base_rest.http')
     def test_session_expired(self):
         response = self.url_open('%s/session_expired' % self.url, "{}")
-        self.assertEqual(response.code, 401)
+        self.assertEqual(response.status_code, 401)
         self.assertEqual(response.headers['content-type'], 'application/json')
-        body = json.loads(response.fp.read())
+        body = json.loads(response.content)
         self.assertDictEqual(
             body,
             {"code": 401,
@@ -67,9 +66,9 @@ class TestException(HttpCase, RegistryMixin):
     @odoo.tools.mute_logger('odoo.addons.base_rest.http')
     def test_missing_error(self):
         response = self.url_open('%s/missing_error' % self.url, "{}")
-        self.assertEqual(response.code, 404)
+        self.assertEqual(response.status_code, 404)
         self.assertEqual(response.headers['content-type'], 'application/json')
-        body = json.loads(response.fp.read())
+        body = json.loads(response.content)
         self.assertDictEqual(
             body,
             {"code": 404,
@@ -79,9 +78,9 @@ class TestException(HttpCase, RegistryMixin):
     @odoo.tools.mute_logger('odoo.addons.base_rest.http')
     def test_access_error(self):
         response = self.url_open('%s/access_error' % self.url, "{}")
-        self.assertEqual(response.code, 403)
+        self.assertEqual(response.status_code, 403)
         self.assertEqual(response.headers['content-type'], 'application/json')
-        body = json.loads(response.fp.read())
+        body = json.loads(response.content)
         self.assertDictEqual(
             body,
             {"code": 403,
@@ -91,9 +90,9 @@ class TestException(HttpCase, RegistryMixin):
     @odoo.tools.mute_logger('odoo.addons.base_rest.http')
     def test_access_denied(self):
         response = self.url_open('%s/access_denied' % self.url, "{}")
-        self.assertEqual(response.code, 403)
+        self.assertEqual(response.status_code, 403)
         self.assertEqual(response.headers['content-type'], 'application/json')
-        body = json.loads(response.fp.read())
+        body = json.loads(response.content)
         self.assertDictEqual(
             body,
             {"code": 403,
@@ -103,17 +102,17 @@ class TestException(HttpCase, RegistryMixin):
     @odoo.tools.mute_logger('odoo.addons.base_rest.http')
     def test_http_exception(self):
         response = self.url_open('%s/http_exception' % self.url, "{}")
-        self.assertEqual(response.code, 405)
+        self.assertEqual(response.status_code, 405)
         self.assertEqual(response.headers['content-type'], 'text/html')
-        body = response.fp.read()
-        self.assertIn('Method Not Allowed', body)
+        body = response.content
+        self.assertIn(b'Method Not Allowed', body)
 
     @odoo.tools.mute_logger('odoo.addons.base_rest.http')
     def test_bare_exception(self):
         response = self.url_open('%s/bare_exception' % self.url, "{}")
-        self.assertEqual(response.code, 500)
+        self.assertEqual(response.status_code, 500)
         self.assertEqual(response.headers['content-type'], 'application/json')
-        body = json.loads(response.fp.read())
+        body = json.loads(response.content)
         self.assertDictEqual(
             body,
             {"code": 500,
