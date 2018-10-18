ac6b92d0e28e3a09ddb35593110fbbe4b66889c2
Leonardo Donelli <learts92@gmail.com>
2018-10-18 23:09:18 +0200

    Support nested fields in fields map of rest.mixin models
    Walk through any numbers of relations in fields map, allowing you to export
    any related field.
    For example, if defining rest.mixin for project.task, you could set a
    fields map like this to export the name of the customer of the project:
    
    _rest_fields_map = {
        'project_id.partner_id.name': 'customerName'
    }

--- rest_model_mixin.py
+++ rest_model_mixin.py
@@ -18,6 +18,16 @@ class RESTModelMixin(models.AbstractModel):
         self.build_inverse_fields_map()
         return res
 
+    def _get_field_desc(self, field_path):
+        record = self
+        field_components = field_path.split('.')
+        relations, field_name = field_components[:-1], field_components[-1]
+        for relation in relations:
+            record = record[relation]
+        return (
+            record._fields[field_name].type,
+            record[field_name])
+
     def build_default_rest_fields_map(self):
         cls = type(self)
         cls._rest_fields_map = {
@@ -31,16 +41,16 @@ class RESTModelMixin(models.AbstractModel):
         }
 
     def serialize_field(self, field_name):
-        field_type = self._fields[field_name].type
+        field_type, field_value = self._get_field_desc(field_name)
         if field_type in self.BASE_FIELD_TYPES:
             # Use false only for boolean fields, convert to None otherwise
-            if self[field_name] is False:
+            if field_value is False:
                 return False if field_type == 'boolean' else None
-            return self[field_name]
+            return field_value
         if field_type == 'many2one':
-            return self[field_name].id if self[field_name] else None
+            return field_value.id if field_value else None
         if field_type in ('one2many', 'many2many'):
-            return self[field_name].ids
+            return field_value.ids
 
     def to_json_field_name(self, field_name):
         return self._rest_fields_map.get(field_name, field_name)
