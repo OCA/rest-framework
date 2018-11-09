0cc25caee570aec0ef6483db6da0d967989d8330
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2018-11-09 08:42:44 +0100

    [MIG] base_rest, base_rest_demo: Migration to 12.0

--- base_rest_demo/__manifest__.py
+++ base_rest_demo/__manifest__.py
@@ -5,7 +5,7 @@
     'name': 'Base Rest Demo',
     'summary': """
         Demo addon for Base REST""",
-    'version': '11.0.1.0.0',
+    'version': '12.0.1.0.0',
     'development_status': 'Beta',
     'license': 'AGPL-3',
     'author': 'ACSONE SA/NV, '
