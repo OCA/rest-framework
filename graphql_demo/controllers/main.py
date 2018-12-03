1006ef9c576b4bb8091c49120d078acbc39da79f
Stéphane Bidoul (ACSONE) <stephane.bidoul@acsone.eu>
2018-12-03 18:11:08 +0100

    graphql: more documentation

--- graphql_demo/controllers/main.py
+++ graphql_demo/controllers/main.py
@@ -8,15 +8,20 @@ from ..schema import schema
 
 
 class GraphQLController(http.Controller, GraphQLControllerMixin):
+
+    # The GraphiQL route, providing an IDE for developers
     @http.route("/graphiql/demo", auth="user")
     def graphiql(self, **kwargs):
         return self._handle_graphiql_request(schema)
 
-    # optional, needed to accept application/json GraphQL requests
-    # if only accepting GET/POST requests, this is not necessary
-    # see also
+    # Optional monkey patch, needed to accept application/json GraphQL
+    # requests. If you only need to accept GET/POST requests,
+    # this is not necessary.
     GraphQLControllerMixin.patch_for_json("^/graphql/demo/?$")
 
+    # The graphql route, for applications.
+    # Note csrf=False: you may want to apply extra security
+    # (such as origin restrictions) to this route.
     @http.route("/graphql/demo", auth="user", csrf=False)
     def graphql(self, **kwargs):
         return self._handle_graphql_request(schema)
