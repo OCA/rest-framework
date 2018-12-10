e0fb8a85fe8968c6b0d34ee47d50c820a22a94cd
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2018-12-10 19:57:51 +0100

    [REL] 11.0.1.0.1

--- base_rest/__manifest__.py
+++ base_rest/__manifest__.py
@@ -6,7 +6,7 @@
     'summary': """
         Develop your own high level REST APIs for Odoo thanks to this addon.
         """,
-    'version': '11.0.1.0.0',
+    'version': '11.0.1.0.1',
     "development_status": "Beta",
     'license': 'AGPL-3',
     'author': 'ACSONE SA/NV, '
