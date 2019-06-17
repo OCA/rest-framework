8746e91078a1b569a5eb8c8e489ef3f86759b094
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2019-06-17 10:17:50 +0200

    [CHG] base_rest: Use more permissive licence: AGPL-> LGPL This change will allow to use this framwork addon to build services on top of OE and LGPL addons

--- base_rest_demo/__manifest__.py
+++ base_rest_demo/__manifest__.py
@@ -1,5 +1,5 @@
 # Copyright 2018 ACSONE SA/NV
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 {
     'name': 'Base Rest Demo',
@@ -7,7 +7,7 @@
         Demo addon for Base REST""",
     'version': '12.0.1.0.0',
     'development_status': 'Beta',
-    'license': 'AGPL-3',
+    'license': 'LGPL-3',
     'author': 'ACSONE SA/NV, '
               'Odoo Community Association (OCA)',
     "maintainers": ['lmignon'],
