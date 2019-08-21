7e8523fa356df09f0c7e3220841836f38ac8ef0f
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2019-08-21 09:48:52 +0200

    [ADD] datamodel: New addon allowing you to define dataclass like models serialisable to/from json/yaml

---
+++ datamodel/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2019 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Datamodel",
+    "summary": """
+        This addon allows you to define simple data models supporting
+        serialization/deserialization""",
+    "version": "12.0.1.0.0",
+    "license": "AGPL-3",
+    "development_status": "Beta",
+    "author": "ACSONE SA/NV, " "Odoo Community Association (OCA)",
+    "maintainers": ["lmignon"],
+    "website": "https://acsone.eu/",
+    "depends": [],
+    "data": [],
+    "demo": [],
+    "external_dependencies": {"python": ["marshmallow", "marshmallow_objects"]},
+}
