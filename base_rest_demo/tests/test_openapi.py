e8248afa6f934e12d0b044782fa494fd940fe464
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2019-10-17 12:36:53 +0200

    black, pre-commit

--- base_rest_demo/tests/test_openapi.py
+++ base_rest_demo/tests/test_openapi.py
@@ -7,38 +7,30 @@ from .common import CommonCase, get_canonical_json
 
 
 class TestOpenAPI(CommonCase):
-
     def _fix_server_url(self, openapi_def):
         # The server url depends of base_url. base_url depends of the odoo
         # config
-        url = openapi_def['servers'][0]['url']
-        url.replace('http://localhost:8069', self.base_url)
-        openapi_def['servers'][0]['url'] = url
+        url = openapi_def["servers"][0]["url"]
+        url.replace("http://localhost:8069", self.base_url)
+        openapi_def["servers"][0]["url"] = url
 
     def test_partner_api(self):
-        partner_service = self.private_services_env.component(
-            usage="partner")
+        partner_service = self.private_services_env.component(usage="partner")
         openapi_def = partner_service.to_openapi()
         canonical_def = get_canonical_json("partner_api.json")
         self._fix_server_url(canonical_def)
-        self.assertFalse(
-            jsondiff.diff(openapi_def, canonical_def))
+        self.assertFalse(jsondiff.diff(openapi_def, canonical_def))
 
     def test_ping_api(self):
-        ping_service = self.public_services_env.component(
-            usage="ping"
-        )
+        ping_service = self.public_services_env.component(usage="ping")
         openapi_def = ping_service.to_openapi()
         canonical_def = get_canonical_json("ping_api.json")
         self._fix_server_url(canonical_def)
-        self.assertFalse(
-            jsondiff.diff(openapi_def, canonical_def))
+        self.assertFalse(jsondiff.diff(openapi_def, canonical_def))
 
     def test_partner_image_api(self):
-        partner_service = self.private_services_env.component(
-            usage="partner_image")
+        partner_service = self.private_services_env.component(usage="partner_image")
         openapi_def = partner_service.to_openapi()
         canonical_def = get_canonical_json("partner_image_api.json")
         self._fix_server_url(canonical_def)
-        self.assertFalse(
-            jsondiff.diff(openapi_def, canonical_def))
+        self.assertFalse(jsondiff.diff(openapi_def, canonical_def))
