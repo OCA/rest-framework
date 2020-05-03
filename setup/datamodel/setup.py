f68a03ff37f8874652bf1866acc73bd9382204d4
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2020-05-03 18:37:45 +0200

    [IMP] datamodel: marshmallow 2.0 is released

--- setup/datamodel/setup.py
+++ setup/datamodel/setup.py
@@ -4,7 +4,7 @@ setuptools.setup(
     setup_requires=['setuptools-odoo'],
     odoo_addon={
         "external_dependencies_override": {
-            "python": {"marshmallow_objects": "marshmallow-objects>=1.0.21"}
+            "python": {"marshmallow_objects": "marshmallow-objects>=2.0.0"}
         }
     },
 )
