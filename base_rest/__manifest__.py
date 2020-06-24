8f5121d0f32de18b3c12054368968480402fd451
KevinKhao <kevinkhao@gmail.com>
2020-06-25 00:12:18 +0200

    Minor dependency on datamodel fix

--- base_rest/__manifest__.py
+++ base_rest/__manifest__.py
@@ -12,7 +12,7 @@
     "author": "ACSONE SA/NV, " "Odoo Community Association (OCA)",
     "maintainers": ["lmignon"],
     "website": "https://github.com/OCA/rest-framework",
-    "depends": ["component"],
+    "depends": ["component", "datamodel"],
     "data": [
         "views/assets_template.xml",
         "views/openapi_template.xml",
