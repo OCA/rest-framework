220c3a8998e3c549feea1834c8aa5605078d048d
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2020-08-19 15:46:57 +0200

    Pin graphene<3
    Graphene >= 3 dependes on graphql-core > 3,
    which is incompatible with graphql-server-core.

--- setup/graphql_base/setup.py
+++ setup/graphql_base/setup.py
@@ -1,10 +1,13 @@
 import setuptools
 
 setuptools.setup(
-    setup_requires=["setuptools-odoo"],
+    setup_requires=['setuptools-odoo'],
     odoo_addon={
         "external_dependencies_override": {
-            "python": {"graphql_server": "graphql-server-core"}
+            "python": {
+                "graphql_server": "graphql-server-core",
+                "graphene": "graphene<3",
+            }
         }
     },
 )
