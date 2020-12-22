45ed2ed188104a1730ff135578b040a0e86e2eeb
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2020-12-22 13:46:53 +0100

    [FIX] base_rest_datamodel: Upgrade to 13.0

--- base_rest_datamodel/__manifest__.py
+++ base_rest_datamodel/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Base Rest Datamodel",
     "summary": """
         Datamodel binding for base_rest""",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "license": "AGPL-3",
     "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
     "website": "https://acsone.eu/",
