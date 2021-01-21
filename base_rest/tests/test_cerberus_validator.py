0c4983c6ce811ec2bd16282036fab49e1cce2d37
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2021-01-21 14:45:26 +0100

    Add direction in methods to get validator schema
    For input (request) / output (response) validation schemas.
    This direction is passed to the component BaseRestCerberusValidator,
    which can be customized to use another strategy to retrieve the
    validator (such as delegating to dedicated components).
    
    The direction is not used in the default implementation as the
    validation schema is get from the passed method name on the service.
    
    An implementation using it could be: when the direction is "input", look
    for a method name on a component named <service>.input and when the
    direction is "output", look for a component named <service>.output.

--- base_rest/tests/test_cerberus_validator.py
+++ base_rest/tests/test_cerberus_validator.py
@@ -277,7 +277,7 @@ class TestCerberusValidator(unittest.TestCase, MetaCase("DummyCase", (object,), 
                 return BaseRestCerberusValidator(unittest.mock.Mock())
 
         v = CerberusValidator(schema="_get_simple_schema")
-        validator = v.get_cerberus_validator(MyService())
+        validator = v.get_cerberus_validator(MyService(), "output")
         self.assertTrue(validator)
         self.assertDictEqual(
             validator.root_schema.schema,
@@ -295,5 +295,34 @@ class TestCerberusValidator(unittest.TestCase, MetaCase("DummyCase", (object,), 
                 return BaseRestCerberusValidator(unittest.mock.Mock())
 
         v = CerberusValidator(schema="_get_simple_schema")
-        validator = v.get_cerberus_validator(MyService())
+        validator = v.get_cerberus_validator(MyService(), "input")
+        self.assertTrue(validator.require_all)
+
+    def test_custom_validator_handler(self):
+        assertEq = self.assertEqual
+
+        class CustomBaseRestCerberusValidator(BaseRestCerberusValidator):
+            def get_validator_handler(self, service, method_name, direction):
+                # In your implementation, this is where you can handle how the
+                # validator is retrieved / computed (dispatch to dedicated
+                # components...).
+                assertEq(service, my_service)
+                assertEq(method_name, "my_endpoint")
+                assertEq(direction, "input")
+                # A callable with no parameter is expected.
+                return lambda: Validator(
+                    {"name": {"type": "string", "required": False}}, require_all=True
+                )
+
+            def has_validator_handler(self, service, method_name, direction):
+                return True
+
+        class MyService(object):
+            def component(self, *args, **kwargs):
+                return CustomBaseRestCerberusValidator(unittest.mock.Mock())
+
+        my_service = MyService()
+
+        v = CerberusValidator(schema="my_endpoint")
+        validator = v.get_cerberus_validator(my_service, "input")
         self.assertTrue(validator.require_all)
