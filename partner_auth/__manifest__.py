b218e34bd6a7080a7ccf00d521952147c7485985
KevinKhao <kevinkhao@gmail.com>
2021-01-31 18:48:17 +0100

    [IMP] Add send invitation email functionality, add tests, misc. minor fixes

--- partner_auth/__manifest__.py
+++ partner_auth/__manifest__.py
@@ -9,7 +9,7 @@
     "license": "AGPL-3",
     "author": "Akretion,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/rest-authenticable",
-    "depends": ["component", "base_rest_datamodel"],
+    "depends": ["component", "base_rest_datamodel", "mail"],
     "data": [
         "security/res_group.xml",
         "security/ir.model.access.csv",
