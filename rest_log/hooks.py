aa8d07458eaaad502f6644119d3ee5d09cad7e6e
Simone Orsi <simahawk@gmail.com>
2021-02-16 12:42:10 +0100

    rest_log: fix init hook
    Previous hook was cloning the table
    but by doing so SQL sequences and indexes were missing.

--- rest_log/hooks.py
+++ rest_log/hooks.py
@@ -5,17 +5,50 @@ import logging
 _logger = logging.getLogger(__name__)
 
 
-def pre_init_hook(cr):
-    """On first install copy recods from shopfloor_log table if available.
-
+def post_init_hook(cr, version):
+    """Preserve log entries from old implementation in shopfloor.
     """
-    cr.execute("SELECT 1 FROM pg_class WHERE relname = 'rest_log'")
-    if cr.fetchone():
-        # rest_log was already installed
-        return
     cr.execute("SELECT 1 FROM pg_class WHERE relname = 'shopfloor_log'")
     if not cr.fetchone():
         # shopfloor_log was already removed
         return
+
     _logger.info("Copy shopfloor.log records to rest.log")
-    cr.execute("CREATE TABLE rest_log AS TABLE shopfloor_log")
+    cr.execute(
+        """
+    INSERT INTO rest_log (
+        request_url,
+        request_method,
+        params,
+        headers,
+        result,
+        error,
+        exception_name,
+        exception_message,
+        state,
+        severity,
+        create_uid,
+        create_date,
+        write_uid,
+        write_date
+    )
+    SELECT
+        request_url,
+        request_method,
+        params,
+        headers,
+        result,
+        error,
+        exception_name,
+        exception_message,
+        state,
+        severity,
+        create_uid,
+        create_date,
+        write_uid,
+        write_date
+    FROM shopfloor_log;
+    """
+    )
+    _logger.info("Delete legacy records in shopfloor_log")
+    cr.execute("""DELETE FROM shopfloor_log""")
