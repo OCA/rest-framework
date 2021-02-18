c60ee15e49464713b7ccefc264846dcebfc9a9fd
Sébastien BEAU <sebastien.beau@akretion.com>
2021-02-18 11:53:35 +0100

    [BUG] issue with datamodel, recursion limit

--- base_rest_demo/tests/__init__.py
+++ base_rest_demo/tests/__init__.py
@@ -1,3 +1,4 @@
 from . import test_controller
 from . import test_openapi
 from . import test_exception
+from . import test_new_api
---
+++ base_rest_demo/tests/test_new_api.py
@@ -0,0 +1,14 @@
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from .common import NewServiceCommonCase
+
+
+class TestPartner(NewServiceCommonCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.service = cls._get_service("partner")
+
+    def test_search(self):
+        self.env["res.partner"].create([{"name": f"P{idx}"} for idx in range(0, 1000)])
+        self.service.dispatch("search")
