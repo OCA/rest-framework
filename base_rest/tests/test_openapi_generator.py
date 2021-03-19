9bb8c4e64b2cd9ab6c06aba7a094dec641837929
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2021-03-19 10:04:34 +0100

    [FIX] base_rest: Includes default parameters and responses into the generated openapi doc fixes #103

--- base_rest/tests/test_openapi_generator.py
+++ base_rest/tests/test_openapi_generator.py
@@ -150,3 +150,57 @@ class TestOpenAPIGenerator(TransactionRestServiceRegistryCase):
                 "schema": {"type": "integer", "format": "int32"},
             },
         )
+
+    def test_03(self):
+        """Test default parameters and default responses
+
+        You can define default parameters and responses at service level.
+        In this test we check that these parameters and responses are into the
+        openapi specification
+        """
+        default_params = [
+            {
+                "name": "API-KEY",
+                "in": "header",
+                "description": "API key for Authorization",
+                "required": True,
+                "schema": {"type": "string"},
+                "style": "simple",
+            }
+        ]
+
+        # pylint: disable=R7980
+        class PartnerService(Component):
+            _inherit = "base.rest.service"
+            _name = "test.partner.service"
+            _usage = "partner"
+            _collection = self._collection_name
+            _description = "Sercice description"
+
+            @restapi.method(
+                [(["/<int:id>/update_name/<string:name>"], "POST")], auth="public"
+            )
+            def update_name(self, _id, _name):
+                """update_name"""
+
+            def _get_openapi_default_parameters(self):
+                defaults = super()._get_openapi_default_parameters()
+                defaults.extend(default_params)
+                return defaults
+
+            def _get_openapi_default_responses(self):
+                responses = super()._get_openapi_default_responses().copy()
+                responses["999"] = "TEST"
+                return responses
+
+        self._build_services(self, PartnerService)
+        service = self._get_service_component(self, "partner")
+        openapi = service.to_openapi()
+        paths = openapi["paths"]
+        self.assertIn("/{id}/update_name/{name}", paths)
+        path = paths["/{id}/update_name/{name}"]
+        self.assertIn("post", path)
+        parameters = path["post"].get("parameters", [])
+        self.assertListEqual(parameters, default_params)
+        responses = path["post"].get("responses", [])
+        self.assertIn("999", responses)
