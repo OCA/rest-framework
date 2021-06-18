ae3962a6236524a3461fb7b9180a0ad008fcae43
David James <david@djdc.net.au>
2021-06-18 15:49:18 +1000

    datamodel: Use more permissive licence: AGPL-> LGPL

--- datamodel/fields.py
+++ datamodel/fields.py
@@ -1,5 +1,5 @@
 # Copyright 2019 ACSONE SA/NV
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html)
 """
 
 Fields
