d2529e51e77e0a9847ff5e421ad297606eff6b11
David James <david@djdc.net.au>
2021-06-18 15:49:18 +1000

    datamodel: Use more permissive licence: AGPL-> LGPL

--- datamodel/builder.py
+++ datamodel/builder.py
@@ -1,6 +1,6 @@
 # Copyright 2017 Camptocamp SA
 # Copyright 2019 ACSONE SA/NV
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html)
 
 """
 
