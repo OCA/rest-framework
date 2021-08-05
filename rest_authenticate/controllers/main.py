775d66092cffaf456b4c8ac5eba4d851f70d0e15
François Degrave <fdegrave@gmail.com>
2021-08-05 17:12:47 +0200

    [IMP] rest_authenticate: remove _default_auth
    Co-authored-by: Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>

--- rest_authenticate/controllers/main.py
+++ rest_authenticate/controllers/main.py
@@ -4,4 +4,3 @@ from odoo.addons.base_rest.controllers import main
 class SessionRestController(main.RestController):
     _root_path = "/session/"
     _collection_name = "session.rest.services"
-    _default_auth = "public"
