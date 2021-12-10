4c34924fb75328772a102fe98a0cb90a00c5006c
Quentin Groulard <quentin.groulard@acsone.eu>
2021-12-10 16:22:20 +0100

    [MIG] Migration to version 14.0: graphql_base

--- graphql_base/__manifest__.py
+++ graphql_base/__manifest__.py
@@ -5,14 +5,14 @@
     "name": "Graphql Base",
     "summary": """
         Base GraphQL/GraphiQL controller""",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "license": "LGPL-3",
     "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/rest-framework",
     "depends": ["base"],
     "data": ["views/graphiql.xml"],
-    "external_dependencies": {"python": ["graphene", "graphql-server-core"]},
-    "development_status": "Beta",
+    "external_dependencies": {"python": ["graphene", "graphql_server"]},
+    "development_status": "Production/Stable",
     "maintainers": ["sbidoul"],
-    "installable": False,
+    "installable": True,
 }
