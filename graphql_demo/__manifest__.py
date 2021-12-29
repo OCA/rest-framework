835ad6b603cdcef9c6f6c1c9e610903c82085202
Maksym Yankin <yankinmk@gmail.com>
2021-12-29 17:11:04 +0200

    graphql_demo: [IMP] black, isort, prettier

--- graphql_demo/__manifest__.py
+++ graphql_demo/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "GraphQL Demo",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "license": "LGPL-3",
     "author": "ACSONE SA/NV, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/rest-framework",
