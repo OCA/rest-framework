46d27092bc0c1be06e19532722f65207b9b32c4c
Mo.Yao <Mo.Yao@misslt.com>
2022-04-06 13:54:05 +0800

    change the behavior of the json request response when the request json body is empty. eg., GET /test but body is empty. Before, it will response 400 error, I think it wasn't properly. After, it will ignore the error, and response the data

--- base_rest/http.py
+++ base_rest/http.py
@@ -123,7 +123,6 @@ class HttpRestRequest(HttpRequest):
             except ValueError as e:
                 msg = "Invalid JSON data: %s" % str(e)
                 _logger.info("%s: %s", self.httprequest.path, msg)
-                raise BadRequest(msg)
         else:
             # We reparse the query_string in order to handle data structure
             # more information on https://github.com/aventurella/pyquerystring
