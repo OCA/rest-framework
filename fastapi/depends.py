f2f56ac95c9f51cd001812dd160564e77be329d5
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2022-09-27 13:51:28 +0200

    [IMP] fastapi: Uses contextvars to hold and provide odoo env on fastapi service excution

--- fastapi/depends.py
+++ fastapi/depends.py
@@ -1,8 +1,7 @@
 from odoo.api import Environment
-from odoo.http import request
+
+from .context import odoo_env_ctx
 
 
 def odoo_env() -> Environment:
-    # TODO the env must be retreaved from contextvar to ensure that
-    # it's properly shared with the thread runner in aswgi
-    yield request.env
+    yield odoo_env_ctx.get()
