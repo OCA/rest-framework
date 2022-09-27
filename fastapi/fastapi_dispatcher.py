64c658a918a331959f99b568011ac6a9e8e57df5
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2022-09-27 13:51:28 +0200

    [IMP] fastapi: Uses contextvars to hold and provide odoo env on fastapi service excution

--- fastapi/fastapi_dispatcher.py
+++ fastapi/fastapi_dispatcher.py
@@ -1,8 +1,9 @@
+from contextlib import contextmanager
 from io import BytesIO
 
 from odoo.http import Dispatcher, request
 
-middelware = None
+from .context import odoo_env_ctx
 
 
 class FastApiDispatcher(Dispatcher):
@@ -21,11 +22,12 @@ class FastApiDispatcher(Dispatcher):
         # depends method
         app = request.env["fastapi.app"].sudo().get_app(root_path)
         data = BytesIO()
-        for r in app(environ, self._make_response):
-            data.write(r)
-        return self.request.make_response(
-            data.getvalue(), headers=self.headers, status=self.status
-        )
+        with self._manage_odoo_env():
+            for r in app(environ, self._make_response):
+                data.write(r)
+            return self.request.make_response(
+                data.getvalue(), headers=self.headers, status=self.status
+            )
 
     def handle_error(self, exc):
         pass
@@ -38,3 +40,11 @@ class FastApiDispatcher(Dispatcher):
         environ = self.request.httprequest.environ
         environ["wsgi.input"] = self.request.httprequest._get_stream_for_parsing()
         return environ
+
+    @contextmanager
+    def _manage_odoo_env(self):
+        token = odoo_env_ctx.set(request.env)
+        try:
+            yield
+        finally:
+            odoo_env_ctx.reset(token)
