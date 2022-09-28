f104eecf9c7549700f974c666d6ba59f339ced9a
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2022-09-28 08:54:28 +0200

    [IMP] fastapi: renames fastapi.app into fastapi.endpoint

--- fastapi/__manifest__.py
+++ fastapi/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Odoo FastAPI",
     "summary": """
-        Odoo FastApi instegration""",
+        Odoo FastAPI endpoint""",
     "version": "16.0.0.0.1",
     "license": "LGPL-3",
     "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
@@ -13,11 +13,11 @@
     "depends": ["endpoint_route_handler"],
     "data": [
         "security/res_groups.xml",
-        "security/fastapi_app.xml",
+        "security/fastapi_endpoint.xml",
         "views/fastapi_menu.xml",
-        "views/fastapi_app.xml",
+        "views/fastapi_endpoint.xml",
     ],
-    "demo": ["demo/fastapi_app_demo.xml"],
+    "demo": ["demo/fastapi_endpoint_demo.xml"],
     "external_dependencies": {
         "python": ["fastapi", "python-multipart", "ujson", "a2wsgi"]
     },
