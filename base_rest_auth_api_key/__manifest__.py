0a00e0df299622e503c1cccdaa785833effd3fed
Aiendry Sarkar <aiendry@aktivsoftware.com>
2022-12-05 16:43:29 +0530

    [MIG] base_rest_auth_api_key: Migration to 16.0

--- base_rest_auth_api_key/__manifest__.py
+++ base_rest_auth_api_key/__manifest__.py
@@ -6,17 +6,15 @@
     "summary": """
         Base Rest: Add support for the auth_api_key security policy into the
         openapi documentation""",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "license": "LGPL-3",
     "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/rest-framework",
     "depends": ["base_rest", "auth_api_key"],
     "maintainers": ["lmignon"],
-    "installable": False,
+    "installable": True,
     "auto_install": True,
     "external_dependencies": {
-        "python": [
-            "apispec>=4.0.0",
-        ]
+        "python": ["apispec"],
     },
 }
