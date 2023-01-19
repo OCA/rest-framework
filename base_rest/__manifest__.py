d46c5d63f2b878c61a3300f6564c0a8e6b747147
Arif Mustaqim <68309923+arif-m150@users.noreply.github.com>
2023-01-19 17:06:25 +0700

    [FIX] version in the manifest
    Update version number to 16

--- base_rest/__manifest__.py
+++ base_rest/__manifest__.py
@@ -6,7 +6,7 @@
     "summary": """
         Develop your own high level REST APIs for Odoo thanks to this addon.
         """,
-    "version": "15.0.1.2.0",
+    "version": "16.0.1.2.0",
     "development_status": "Beta",
     "license": "LGPL-3",
     "author": "ACSONE SA/NV, " "Odoo Community Association (OCA)",
