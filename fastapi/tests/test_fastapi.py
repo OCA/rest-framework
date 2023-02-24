e510d123acdc451a3e5f7cee9ea7a0fbc347f402
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2023-02-24 16:26:38 +0100

    [FIX] fastapi: Uses new implementation of endpoint_route_handler

--- fastapi/tests/test_fastapi.py
+++ fastapi/tests/test_fastapi.py
@@ -12,22 +12,10 @@ class FastAPIHttpCase(HttpCase):
     def setUp(self):
         super().setUp()
         self.fastapi_demo_app = self.env.ref("fastapi.fastapi_endpoint_demo")
+        self.fastapi_demo_app._handle_registry_sync()
 
     def test_call(self):
         route = "/fastapi_demo/"
         response = self.url_open(route)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.content, b'{"Hello":"World"}')
-
-    def test_update_route(self):
-        route = "/fastapi_demo/"
-        response = self.url_open(route)
-        self.assertEqual(response.status_code, 200)
-        response = self.url_open("/new_root/")
-        self.assertEqual(response.status_code, 404)
-        self.fastapi_demo_app.root_path = "/new_root"
-        self.env.flush_all()
-        response = self.url_open(route)
-        self.assertEqual(response.status_code, 404)
-        response = self.url_open("/new_root/")
-        self.assertEqual(response.status_code, 200)
