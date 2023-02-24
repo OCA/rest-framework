a12658657a101467a82eae298c49dac57a787877
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2023-02-24 18:40:22 +0100

    [ADD] extendable_fastapi: New addon to allow the use of extendable within fastapi apps

---
+++ extendable_fastapi/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2023 ACSONE SA/NV
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Extendable Fastapi",
+    "summary": """
+        Allows the use of extendable into fastapi apps""",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
+    "maintainers": ["lmignon"],
+    "website": "https://github.com/OCA/rest-framework",
+    "depends": ["fastapi", "extendable"],
+    "data": [],
+    "demo": [],
+    "installable": True,
+}
