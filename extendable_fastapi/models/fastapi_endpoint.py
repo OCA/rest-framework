7e12401217b0086c750a09ace7811f996d716b4c
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2023-02-26 14:06:57 +0100

    [IMP] extendable_fastapi: Relicence to lgpl

--- extendable_fastapi/models/fastapi_endpoint.py
+++ extendable_fastapi/models/fastapi_endpoint.py
@@ -1,5 +1,5 @@
 # Copyright 2023 ACSONE SA/NV
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
 
 from typing import List
 
