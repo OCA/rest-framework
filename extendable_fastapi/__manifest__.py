ce76b55a1371a27b32749f3f228c899fc2dc802a
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2023-02-26 14:06:57 +0100

    [IMP] extendable_fastapi: Relicence to lgpl

--- extendable_fastapi/__manifest__.py
+++ extendable_fastapi/__manifest__.py
@@ -1,12 +1,12 @@
 # Copyright 2023 ACSONE SA/NV
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
 
 {
     "name": "Extendable Fastapi",
     "summary": """
         Allows the use of extendable into fastapi apps""",
     "version": "16.0.1.0.0",
-    "license": "AGPL-3",
+    "license": "LGPL-3",
     "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
     "maintainers": ["lmignon"],
     "website": "https://github.com/OCA/rest-framework",
