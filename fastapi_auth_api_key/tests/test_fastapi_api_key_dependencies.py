34776c1cae740b84360073c14b2aa557035f2c4e
sonhd91 <sonhd@trobz.com>
2024-05-24 18:23:29 +0700

    [MIG] fastapi_auth_api_key: Migration to 17.0

--- fastapi_auth_api_key/tests/test_fastapi_api_key_dependencies.py
+++ fastapi_auth_api_key/tests/test_fastapi_api_key_dependencies.py
@@ -17,8 +17,17 @@ class TestFastapiAuthApiKey(TransactionCase):
         super().setUpClass()
         cls.env = cls.env(context=dict(cls.env.context, tracking_disable=True))
         # Is it valid? We need an env without superpowers
-        demo_user = cls.env.ref("base.partner_demo")
+        demo_user = cls.env.ref("base.user_demo")
         cls.demo_env = demo_user.with_user(demo_user).env
+        cls.demo_endpoint = cls.env["fastapi.endpoint"].create(
+            {
+                "name": "Test Enpoint",
+                "app": "demo",
+                "root_path": "/path_demo",
+                "demo_auth_method": "api_key",
+                "user_id": demo_user.id,
+            }
+        )
         cls.setUpClassApiKey()
 
     @classmethod
@@ -63,19 +72,22 @@ class TestFastapiAuthApiKey(TransactionCase):
     def test_authenticated_auth_api_key(self):
         # An exception is raised when no api key is used
         with self.assertRaises(HTTPException) as error:
-            authenticated_auth_api_key(False, self.demo_env)
+            authenticated_auth_api_key(False, self.demo_env, self.demo_endpoint)
         self.assertEqual(error.exception.detail, "No HTTP-API-KEY provided")
         # An exception is raised when no api key record is found
         with self.assertRaises(HTTPException) as error:
-            authenticated_auth_api_key("404", self.demo_env)
-        self.assertEqual(error.exception.detail, "The key 404 is not allowed")
+            authenticated_auth_api_key("404", self.demo_env, self.demo_endpoint)
+        self.assertEqual(error.exception.detail, ("The key 404 is not allowed",))
         # TODO enable this when we know how to filter keys based
         # on endpoint's api key group.
         # An exception is raised when unauthorized api key record is found
         # with self.assertRaises(HTTPException) as error:
         # authenticated_auth_api_key("not_authorized", self.demo_env)
+        self.demo_endpoint.auth_api_key_group_id = (
+            self.authorized_api_key.auth_api_key_group_ids[0]
+        )
         result_key = authenticated_auth_api_key(
-            self.authorized_api_key.key, self.demo_env
+            self.authorized_api_key.key, self.demo_env, self.demo_endpoint
         )
         self.assertEqual(result_key, self.authorized_api_key)
 
