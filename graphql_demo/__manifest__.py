3691eaa06fe60f1f73b0fdf12b912c58f82ed58d
Diogo Duarte <dduarte@odoogap.com>
2024-08-19 16:05:19 +0100

    [MIG] graphql_base: Migration to 17.0

--- graphql_demo/__manifest__.py
+++ graphql_demo/__manifest__.py
@@ -3,13 +3,12 @@
 
 {
     "name": "GraphQL Demo",
-    "version": "16.0.1.0.1",
+    "version": "17.0.1.0.1",
     "license": "LGPL-3",
     "author": "ACSONE SA/NV, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/rest-framework",
     "depends": ["graphql_base"],
-    "external_dependencies": {"python": ["graphene"]},
     "development_status": "Beta",
     "maintainers": ["sbidoul"],
-    "installable": False,
+    "installable": True,
 }
