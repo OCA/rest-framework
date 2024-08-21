1b20081066680f35dca5aa4e22f25079c3021ba5
Marko <marko.falatec@ecodica.eu>
2024-08-21 14:04:28 +0200

    [FIX] fastapi: Consolidating licenses inside the fastapi modue to LGPL-3

--- fastapi/schemas.py
+++ fastapi/schemas.py
@@ -1,5 +1,5 @@
 # Copyright 2022 ACSONE SA/NV
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/LGPL).
 import warnings
 from enum import Enum
 from typing import Annotated, Generic, List, Optional, TypeVar
