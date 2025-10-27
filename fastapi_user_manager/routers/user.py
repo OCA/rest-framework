ec043203200a1118d1ef25f77fda5316b94933a4
TB-Ph35 <thomas.bonneru@phelumtech.fr>
2025-10-27 15:23:48 +0100

    add error information

--- fastapi_user_manager/routers/user.py
+++ fastapi_user_manager/routers/user.py
@@ -28,9 +28,12 @@ def create_user_data(
 
     result = {}
     for d in data:
-        user = env["api.user.router"]._write_user(d)
+        user, error = env["api.user.router"]._write_user(d)
         if user:
             result[f"{user.login}"] = "Write ok"
+            if error:
+                for e in error:
+                    result[f"{user.login}/{e}"] = error[e]
         else:
             result[f"{d.login}"] = "Error login"
 
@@ -86,26 +89,28 @@ class ApiUserRouter(models.AbstractModel):
 
     # user = fields.Many2one(comodel_name="res.users")
     def _write_user(self, data: UserSc):
-        vals = self._get_user_values(data)
+        vals, error = self._get_user_values(data)
         user = self.env["res.users"].search([("login", "=", data.login)])
         if user:
-            user.write(vals)
+            user.sudo().write(vals)
+            user, error = self._post_process_user_update(user, data.misc, error)
         else:
-            user = self.env["res.users"].create(vals)
-            user = self._post_process_user_creation(user, data.misc)
-        return user
+            user = self.env["res.users"].sudo().create(vals)
+            user, error = self._post_process_user_creation(user, data.misc, error)
+        return user, error
 
-    def _post_process_user_update(self, user, misc):
+    def _post_process_user_update(self, user, misc, error):
         """inherit it to adapt to your needs"""
-        return user
+        return user, error
 
-    def _post_process_user_creation(self, user, misc):
+    def _post_process_user_creation(self, user, misc, error):
         """inherit it to adapt to your needs"""
-        return user
+        return user, error
 
     def _get_user_values(self, data: UserSc):
         """inherit it to adapt to your needs"""
         vals = data.misc.copy()
+        error = {}
         if "login" not in vals:
             vals["login"] = data.login
         if "company" in vals:
@@ -114,20 +119,31 @@ class ApiUserRouter(models.AbstractModel):
                 [("company_registry", "in", vals_company)]
             )
             if company_id:
+                if len(vals_company) != len(company_id):
+                    list_company_id = []
+                    for x in company_id:
+                        list_company_id.append(x.company_registry)
+                    error["company"] = f"{list_company_id} for {vals_company}"
                 vals["company_ids"] = [Command.link(comp) for comp in company_id.ids]
                 vals["company_id"] = company_id[0].id
             else:
+                error["company"] = "no company found"
                 vals["company_id"] = self.env.company.id
         if "roles" in vals:
             vals_roles = vals.pop("roles")
             if hasattr(self.env["res.users"], "role_ids"):
                 roles = self.env["res.users.role"].search([("name", "in", vals_roles)])
-                vals["role_line_ids"] = [
-                    Command.create({"role_id": rol_id}) for rol_id in roles.ids
-                ]
+                if roles:
+                    if len(vals_roles) != len(roles):
+                        error["roles"] = "not all roles have been found"
+                    vals["role_line_ids"] = [
+                        Command.create({"role_id": rol_id}) for rol_id in roles.ids
+                    ]
+                else:
+                    error["roles"] = "Error no roles found"
             else:
                 raise NotImplementedError
         if "group" in vals:
             vals.pop("group")
             raise NotImplementedError
-        return vals
+        return vals, error
